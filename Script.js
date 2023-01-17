a5b23f53c68f89ebf339224e15ff03d48d5f7c02
Akashshripal <98543501+Akashshripal@users.noreply.github.com>
2023-01-17 21:42:44 +0530

    update

--- Script.js
+++ Script.js
@@ -8,7 +8,7 @@ let alarmTime , isAlarmSet,
     ringtone = new Audio("./files/ringtone.mp3");
     
     for(let i=12; i > 0; i--){
-     i = i < 10 ? `0${i}` : i ;
+     i = i < 10 ? `0${i}` : i ; 
      let option = `<option value = "${i}"> ${i} </option>`;
      selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
     }
